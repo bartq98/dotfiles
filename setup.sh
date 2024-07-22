45e12bef9bacd26cd801f29cc0b7f7a907e1acb5
bartq98 <bartusiekkrosno@poczta.fm>
2024-07-22 03:35:16 +0200

    Add base, untested `setup.sh`
    Should be use for fresh, new installation of Arch Linux.

---
+++ setup.sh
@@ -0,0 +1,36 @@
+#!/bin/env bash
+
+{
+# Colors
+RED="\e[0;31m"
+GREEN="\e[0;32m"
+BLUE="\e[0;34m"
+CLEAR="\e[0m"
+
+rsync_copy='rsync -avz --progress -h'
+
+printf "Installing ${BLUE$}pacman packages{CLEAR} from ./packages/pacman\n"
+sudo pacman -Syyu --noconfirm - < ./packages/pacman
+
+printf "${BLUE}Configuring shell${CLEAR}\n"
+$rsync_copy ./zsh/ $HOME
+
+printf "Installing ${BLUE}Oh My ZSH${CLEAR}\n"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+printf "Installing ${BLUE}Powerlevel10k theme${CLEAR}\n"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+printf "Installing ${BLUE$}zsh syntax highligthing{CLEAR}\n"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+printf "Installing ${BLUE$}zsh-completions{CLEAR}\n"
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
+
+printf "Installing ${BLUE$}zsh-autosuggestions{CLEAR}\n"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+printf "Installing ${BLUE$}fzf-tab{CLEAR}\n"
+git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab
+
+}
