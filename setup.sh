c0323e6535e00e26ab1892899261154b0fc135e3
bartq98 <bartusiekkrosno@poczta.fm>
2024-07-22 03:40:06 +0200

    Add setup $HOME directories

--- setup.sh
+++ setup.sh
@@ -6,8 +6,13 @@ RED="\e[0;31m"
 GREEN="\e[0;32m"
 BLUE="\e[0;34m"
 CLEAR="\e[0m"
-
 rsync_copy='rsync -avz --progress -h'
+
+printf "Configuring $HOME directories"
+mkdir $HOME/Backups
+touch $HOME/Backups/.directory && printf "[Desktop Entry]\nIcon=folder-templates" > $HOME/Backups/.directory
+mkdir $HOME/github/
+touch $HOME/github/.directory && printf "[Desktop Entry]\nIcon=folder-git" > $HOME/github/.directory
 
 printf "Installing ${BLUE$}pacman packages{CLEAR} from ./packages/pacman\n"
 sudo pacman -Syyu --noconfirm - < ./packages/pacman
